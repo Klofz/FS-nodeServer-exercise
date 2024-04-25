a5eab0bc6e7e50cefe5644747553b1154ff0113f
Klofz <141661020+Klofz@users.noreply.github.com>
2024-04-25 17:51:39 -0400

    uibuild-updated

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3901;
 const URL1 = "/api/persons";
 
-app.use(cors());
+app.use(express.static("distr"));
 app.use(express.json());
 
 app.use(formatterMatter);
